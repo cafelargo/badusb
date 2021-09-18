2d2836511870a900da5d6fc64cd3a6ef10fd43ee
Café largo <67487108+cafelargo@users.noreply.github.com>
2021-09-18 12:47:31 +0100

    rick.c changes

--- code-writing.c
+++ code-writing.c
@@ -7,161 +7,148 @@
 //lookup tables for getting index which correlates to the use_index
 static const uint8_t g_lookupTable[] = "\0\0\0\0abcdefghijklmnopqrstuvwxyz1234567890\r\x1b\x7f\t -=[]\\#;'`,./";
 static const uint8_t g_shiftTable[] = "\0\0\0\0ABCDEFGHIJKLMNOPQRSTUVWXYZ!\"\x9c$\x25^&*()\n\0\b\0\0_+{}|~:@\xaa<>?";
-#define CTRL 0b00000001
-#define SHFT 0b00000010
-#define ALT 0b00000100
-#define GUI 0b00001000
-#define ESC 0x1b
-#define DEL 0x7f
 
-//wait for x number of milliseconds using nanosleep
-void delay(long milliseconds)
-{
+enum ModifierKey {
+    NONE = 0,
+    CTRL = 1 << 0,
+    SHIFT = 1 << 1,
+    ALT = 1 << 2,
+    GUI = 1 << 3,
+};
+
+static enum ModifierKey get_modifier(const uint8_t* input, size_t len) {
+    if (len != 5) { return NONE; }
+    uint8_t modifiers[4][5] = {
+            "CTRL", // 1 << 0 = 1
+            "SHFT", // 1 << 1 = 2
+            "ALT\0", // 1 << 2 = 4
+            "GUI\0", // 1 << 3 = 8
+    };
+    uint8_t mask = NONE | CTRL | SHIFT | ALT | GUI;
+    for (uint8_t i = 0; i < 5; i++) {
+        for (uint8_t j = 0; j < 4; j++) {
+            mask &= ~((input[i] != modifiers[j][i]) << j);
+        }
+    }
+    // Must be a single flag because there are no equal strings
+    return (enum ModifierKey) mask;
+}
+
+static void delay(long milliseconds) {
     struct timespec waitTime;
     waitTime.tv_sec = milliseconds / 1000;
     waitTime.tv_nsec = (milliseconds % 1000) * 1000000;
-    nanosleep(&waitTime, &waitTime);
-    return;
+    while (-1 == nanosleep(&waitTime, &waitTime));
 }
 
-void send_code(uint8_t hidCode[9])
-{
-    for (int i = 0; i < 8; i++)
-    {
+static void send_code(uint8_t hidCode[9]) {
+    for (int i = 0; i < 8; i++) {
         printf("|%d|", hidCode[i]);
     }
     printf("\n");
-    FILE *fp;
-    fp = fopen("/dev/hidg0", "wb"); //writes to file which is what's "sent" to pc
+    //writes to file which is what's "sent" to pc
+    FILE* fp;
+    fp = fopen("/dev/hidg0", "wb");
+    if (fp == NULL) {
+        printf("Failed to open /dev/hidg0\n");
+        exit(EXIT_FAILURE);
+    }
     fwrite(hidCode, 8, 1, fp);
     fclose(fp);
-    return;
 }
 
-//the key has to be released after every press if normal keys
-void release_key()
-{
+/// the key has to be released after every press if normal keys
+static void release_key() {
     uint8_t nullArr[9] = "\0\0\0\0\0\0\0\0"; //code sent
     send_code(nullArr);
-    return;
 }
 
-//get the selector id for a character (lower as shift requires modifier)
-uint8_t get_selector_val(uint8_t character)
-{
+/// get the selector id for a character (lower as shift requires modifier)
+static uint8_t get_selector_val(uint8_t character) {
     uint8_t index;
-    for (index = 0; index < sizeof(g_lookupTable); index++) //read thru lookup table for lower (non shift) uint8_ts
-    {
-        if (g_lookupTable[index] == character)
-        {
+    //read thru lookup table for lower (non shift) uint8_ts
+    for (index = 0; index < (uint8_t) sizeof(g_lookupTable); index++) {
+        if (g_lookupTable[index] == character) {
             return index;
         }
     }
     return 255;
 }
 
-//iterates through the string given, applying relevant operations on the HID code depending on what keys are passed in
-void hold_keys(uint8_t keysHeld[])
-{
+/// iterates through the string given, applying relevant operations on the HID code depending on what keys are passed in
+static void hold_keys(char keysHeld[]) {
     uint8_t tmp_key[5] = "\0\0\0\0\0";
-    uint8_t uint8_t_count = 0;
-    uint8_t selector_val = 0;
+    uint8_t char_count = 0;
     uint8_t hidCode[9] = "\0\0\0\0\0\0\0\0";
-    uint8_t currentChar;
-    for (int x = 0; x <= strlen(keysHeld); x++) //iterate through given string
-    {
-        currentChar = keysHeld[x];
-        if(currentChar == ' ' || currentChar  == '\0') //delimited by spaces - so once finds space or \0 then record the key and does operation
-        {
-            uint8_t_count = 0;
-            if (strcmp(tmp_key, "CTRL") == 0) //if string matches one of these codes, perform bitwise or to add modifier bit to the modifier value in hidCode
-            {
-                hidCode[0] |= CTRL;
-            }
-            else if (strcmp(tmp_key, "SHFT") == 0)
-            {
-                hidCode[0] |= SHFT;
-            }
-            else if (strcmp(tmp_key, "ALT") == 0)
-            {
-                hidCode[0] |= ALT;
-            }
-            else if (strcmp(tmp_key, "GUI") == 0)
-            {
-                hidCode[0] |= GUI;
-            }
-            else
-            {
-                selector_val = get_selector_val(tmp_key[0]); //get the selector val for any keys entered - in current condition only one specifier keypress can be done at any one time
-                if (selector_val == 255)
-                {
+    //iterate through given string
+    for (unsigned long x = 0; x < strlen(keysHeld); x++) {
+        char current_char = keysHeld[x];
+        //delimited by spaces, so once finds space or \0 then record the key and does operation
+        if (current_char == ' ' || current_char == '\0') {
+            char_count = 0;
+            // add modifier bit to the modifier value in hidCode
+            enum ModifierKey modifier = get_modifier(tmp_key, 5);
+            hidCode[0] |= modifier;
+            if (modifier == NONE) {
+                //get the selector val for any keys entered - in current condition only one specifier keypress can be done at any one time
+                uint8_t selector_val = get_selector_val(tmp_key[0]);
+                if (selector_val == 255) {
                     printf("There was an incorrect string (%s) sent to the hold_keys function, exiting...\n", tmp_key);
-                    for (int i = 0; i < strlen(tmp_key); i++)
-                    {
-                        printf("<%d>",tmp_key[i]);
+                    for (int i = 0; i < 5; i++) {
+                        printf("<%d>", tmp_key[i]);
                     }
-                    exit(1);
+                    exit(EXIT_FAILURE);
                 }
                 hidCode[2] = selector_val;
             }
-            for (int i = 0; i < 5; i++)
-            {
+            for (int i = 0; i < 5; i++) {
                 tmp_key[i] = '\0';
             }
             x++;
         }
-        tmp_key[uint8_t_count++] = keysHeld[x];
+        tmp_key[char_count++] = (uint8_t)keysHeld[x];
     }
     send_code(hidCode);
-    return;
 }
 
 //put correct values into the array being sent to the target
-void get_array(uint8_t character, uint8_t hidCode[])
-{
+static void get_array(char character, uint8_t hidCode[]) {
     uint8_t index;
-    for (index = 0; index < sizeof(g_lookupTable); index++) //read thru lookup table for lower (non shift) uint8_ts
+    for (index = 0;
+         index < (uint8_t) sizeof(g_lookupTable); index++) //read thru lookup table for lower (non shift) uint8_ts
     {
-        if (g_lookupTable[index] == character)
-        {
+        if (g_lookupTable[index] == character) {
             hidCode[2] = index;
             return;
         }
     }
-    for (index = 0; index < sizeof(g_shiftTable); index++) //read through lookup table for upper (shift) uint8_ts
+    for (index = 0;
+         index < (uint8_t) sizeof(g_shiftTable); index++) //read through lookup table for upper (shift) uint8_ts
     {
-        if (g_shiftTable[index] == character)
-        {
+        if (g_shiftTable[index] == character) {
             hidCode[0] = 32;
             hidCode[2] = index;
             return;
         }
     }
-    return;
 }
 
 //write a single character into hidg0
-void write_character(char character)
-{
+static void write_character(char character) {
     uint8_t hidCode[9] = "\0\0\0\0\0\0\0\0";
     get_array(character, hidCode);
     send_code(hidCode);
     release_key();
-    return;
 }
 
 //iterate through a string and write each letter into hidg0
-void write_string(uint8_t strIn[])
-{
-    for (int i = 0; i < strlen(strIn); i++)
-    {
+static void write_string(char strIn[]) {
+    for (size_t i = 0; i < strlen(strIn); i++) {
         write_character(strIn[i]);
     }
-    return;
 }
 
-int main()
-{
+int main() {
     delay(100);
     hold_keys("GUI r");
     release_key();
@@ -169,5 +156,5 @@ int main()
     write_string("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
     delay(50);
     write_character('\n');
-    return 0;
+    return EXIT_SUCCESS;
 }
